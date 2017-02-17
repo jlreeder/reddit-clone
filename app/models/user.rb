35f436e106a2de41ad95ea3d319cf2afd03e83bb
Jason Reeder <info@jasonreeder.com>
2017-02-17 10:37:38 -0800

    Add User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,32 @@
 #
 
 class User < ActiveRecord::Base
+  after_initialization :ensure_session_token
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: {miminum: 8, allow_nil: true}
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
 
 end
