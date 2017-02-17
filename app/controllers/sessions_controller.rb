8809f48758eb0b4290ad15eae1c5d05d1add3fd1
Jason Reeder <info@jasonreeder.com>
2017-02-17 11:52:38 -0800

    add sign in/log in buttons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    curren_user.reset_session_token!
+    current_user.reset_session_token!
     session[:session_token] = nil
     redirect_to new_session_url
   end
